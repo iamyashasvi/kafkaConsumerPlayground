8b01f6a79107efa0d9c6d8254ac0c7cedcc9206f
Yashasvi <iamyashasvi@gmail.com>
2026-08-02 09:24:03 +0530

    Initial commit for consumer setup

---
+++ src/main/java/com/event_driven_architecture/kafkaConsumerPlayground/KafkaConsumerPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.event_driven_architecture.kafkaConsumerPlayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/event_driven_architecture/kafkaConsumerPlayground/KafkaConsumerPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event_driven_architecture.kafkaConsumerPlayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
